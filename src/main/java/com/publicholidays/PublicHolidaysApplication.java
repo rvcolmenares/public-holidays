a34e2fbe12f87b5adcd849f807897764771fecb3
Rosa Virginia Colmenares <rvcolmenares@gmail.com>
2021-08-12 20:14:52 -0400

    initial commit

---
+++ src/main/java/com/publicholidays/PublicHolidaysApplication.java
@@ -0,0 +1,13 @@
+package com.publicholidays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicHolidaysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicHolidaysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/publicholidays/PublicHolidaysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.publicholidays;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicHolidaysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
