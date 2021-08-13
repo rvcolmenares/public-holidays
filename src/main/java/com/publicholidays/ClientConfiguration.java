333da5bd28799cd3454a0cf060d6ebd87041cfa1
Rosa Virginia Colmenares <rvcolmenares@gmail.com>
2021-08-12 21:08:08 -0400

    cleans appplication code

--- src/main/java/com/publicholidays/ClientConfiguration.java
+++ src/main/java/com/publicholidays/ClientConfiguration.java
@@ -8,8 +8,9 @@ import feign.jackson.JacksonEncoder;
 import feign.okhttp.OkHttpClient;
 import feign.slf4j.Slf4jLogger;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@org.springframework.context.annotation.Configuration
+@Configuration
 public class ClientConfiguration {
 
     @Bean(name = "nagerDateClient")
